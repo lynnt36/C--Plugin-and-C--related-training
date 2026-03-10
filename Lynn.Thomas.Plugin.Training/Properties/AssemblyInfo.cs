9ac3c0ade35afb95a2443c435500eb507cc8b9b1
Lynn Thomas <Lynn.Thomas@DVSA.GOV.UK>
2026-03-10 09:05:22 +0000

    Lesson 1 - plugin basic training

---
+++ Lynn.Thomas.Plugin.Training/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lynn.Thomas.Plugin.Training")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lynn.Thomas.Plugin.Training")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("55d50456-993c-4d2c-9682-461634715e64")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Lynn.Thomas.Plugin.Training/TrainingPlugin.cs
@@ -0,0 +1,48 @@
+﻿// Imports the core .NET library that provides basic functionality used by C# programs
+using System;
+// Imports the Dynamics SDK library, which provides the classes used to build plugins and interact with Dynamics records
+using Microsoft.Xrm.Sdk;
+// A namespace is used to organise code and keep classes grouped together in a project
+namespace Training.Plugin
+{
+    // public  → allows Dynamics to access and run this class
+    // class   → defines a container for our plugin code
+    // : IPlugin → indicates this class implements the Dynamics plugin interface
+    public class TrainingPlugin : IPlugin
+    {
+        // public  → allows Dynamics to access this method
+        // void    → the method performs actions but does not return a value
+        // Execute → the method that Dynamics calls when the plugin is triggered
+        // IServiceProvider serviceProvider → provides access to services like context and tracing
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Get the plugin execution context from Dynamics
+            // This object contains details about the current operation, such as which message triggered the plugin
+            var context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // InputParameters contains data sent to the plugin by Dynamics
+            // "Target" usually represents the record being created or updated
+            // If Target is not present, stop the plugin to prevent errors
+            if (!context.InputParameters.Contains("Target"))
+                return;
+
+            // Get the "Target" record from the InputParameters collection
+            // Convert it to an Entity type so we can access its fields
+            var target = (Entity)context.InputParameters["Target"];
+
+            // target.Contains("name") → checks if the "name" field exists on the record
+            // This prevents errors because Update messages only include fields that changed
+            if (target.Contains("name"))
+            {
+                // GetAttributeValue<string>() → retrieves the field value and converts it to a string
+                // "name" → the logical name of the field in Dynamics
+                var name = target.GetAttributeValue<string>("name");
+            }
+
+            // Throw a plugin exception to stop the record from being saved
+            // The message provided will be shown to the user in Dynamics
+            throw new InvalidPluginExecutionException("Name is required.");
+        }
+    }
+}
